0df7f8aa90832a4e763ec7584322dfcab4221484
Bikash Panda <oksbwn@gmail.com>
2021-07-24 18:48:48 +0000

    Cattegory page partially complete

--- Web App/pages/category/_slug.vue
+++ Web App/pages/category/_slug.vue
@@ -5,7 +5,7 @@
         <div class="col-md-3 col-sm-3 col-xs-12">
           <v-card outlined>
             <v-card-title>Filters</v-card-title>
-            <v-divider></v-divider>
+            <v-divider />
             <template>
               <v-treeview
                 :items="items"
@@ -15,9 +15,9 @@
                 activatable
                 open-on-click
                 dense
-              ></v-treeview>
+              />
             </template>
-            <v-divider></v-divider>
+            <v-divider />
             <v-card-title>Price</v-card-title>
             <v-range-slider
               v-model="range"
@@ -26,7 +26,7 @@
               :height="10"
               class="align-center"
               dense
-            ></v-range-slider>
+            />
             <v-row class="pa-2" dense>
               <v-col cols="12" sm="5">
                 <v-text-field
@@ -35,10 +35,12 @@
                   outlined
                   dense
                   @change="$set(range, 0, $event)"
-                ></v-text-field>
+                />
               </v-col>
               <v-col cols="12" sm="2">
-                <p class="pt-2 text-center">TO</p>
+                <p class="pt-2 text-center">
+                  TO
+                </p>
               </v-col>
               <v-col cols="12" sm="5">
                 <v-text-field
@@ -47,84 +49,137 @@
                   outlined
                   dense
                   @change="$set(range, 1, $event)"
-                ></v-text-field>
+                />
               </v-col>
             </v-row>
-            <v-divider></v-divider>
-            <v-card-title class="pb-0">Customer Rating</v-card-title>
+            <v-divider />
+            <v-card-title class="pb-0">
+              Customer Rating
+            </v-card-title>
             <v-container class="pt-0" fluid>
-              <v-checkbox append-icon="mdi-star" label="4 & above" hide-details dense></v-checkbox>
-              <v-checkbox append-icon="mdi-star" label="3 & above" hide-details dense></v-checkbox>
-              <v-checkbox append-icon="mdi-star" label="2 & above" hide-details dense></v-checkbox>
-              <v-checkbox append-icon="mdi-star" label="1 & above" hide-details dense></v-checkbox>
+              <v-checkbox
+                append-icon="mdi-star"
+                label="4 & above"
+                hide-details
+                dense
+              />
+              <v-checkbox
+                append-icon="mdi-star"
+                label="3 & above"
+                hide-details
+                dense
+              />
+              <v-checkbox
+                append-icon="mdi-star"
+                label="2 & above"
+                hide-details
+                dense
+              />
+              <v-checkbox
+                append-icon="mdi-star"
+                label="1 & above"
+                hide-details
+                dense
+              />
             </v-container>
-            <v-divider></v-divider>
-            <v-card-title class="pb-0">Size</v-card-title>
+            <v-divider />
+            <v-card-title class="pb-0">
+              Size
+            </v-card-title>
             <v-container class="pt-0" fluid>
-              <v-checkbox label="XS" hide-details dense></v-checkbox>
-              <v-checkbox label="S" hide-details dense></v-checkbox>
-              <v-checkbox label="M" hide-details dense></v-checkbox>
-              <v-checkbox label="L" hide-details dense></v-checkbox>
-              <v-checkbox label="XL" hide-details dense></v-checkbox>
-              <v-checkbox label="XXL" hide-details dense></v-checkbox>
-              <v-checkbox label="XXXL" hide-details dense></v-checkbox>
+              <v-checkbox label="XS" hide-details dense />
+              <v-checkbox label="S" hide-details dense />
+              <v-checkbox label="M" hide-details dense />
+              <v-checkbox label="L" hide-details dense />
+              <v-checkbox label="XL" hide-details dense />
+              <v-checkbox label="XXL" hide-details dense />
+              <v-checkbox label="XXXL" hide-details dense />
             </v-container>
           </v-card>
         </div>
         <div class="col-md-9 col-sm-9 col-xs-12">
-          <v-breadcrumbs class="pb-0" :items="breadcrums"></v-breadcrumbs>
-
+          <v-breadcrumbs class="pb-0" :items="breadcrums" />
           <v-row dense>
             <v-col cols="12" sm="8" class="pl-6 pt-6">
-              <small>Showing 1-12 of 200 products</small>
+              <small>Showing {{ (page - 1) * pageSize }}-{{ page * pageSize }} of
+                {{ totalProducts }} products</small>
             </v-col>
             <v-col cols="12" sm="4">
               <v-select
-                class="pa-0"
                 v-model="select"
+                class="pa-0"
                 :items="options"
-                style="margin-bottom: -20px;"
+                style="margin-bottom: -20px"
                 outlined
                 dense
-              ></v-select>
+              />
             </v-col>
           </v-row>
 
-          <v-divider></v-divider>
+          <v-divider class="pb-2" />
 
           <div class="row text-center">
-            <div class="col-md-3 col-sm-6 col-xs-12" :key="pro.id" v-for="pro in products">
-              <v-hover v-slot:default="{ hover }">
+            <div
+              v-for="product in products"
+              :key="product.id"
+              class="col-md-3 col-sm-6 col-xs-12"
+            >
+              <v-hover v-slot="{ hover }">
                 <v-card class="mx-auto" color="grey lighten-4" max-width="600">
                   <v-img
                     class="white--text align-end"
-                    :aspect-ratio="16/9"
+                    :aspect-ratio="16 / 9"
                     height="200px"
-                    :src="pro.src"
+                    :src="`${$axios.defaults.baseURL}/thumbnails/${product.image}`"
+                    lazy-src="img/image_ph.jpg"
                   >
-                    <v-card-title>{{pro.type}}</v-card-title>
+                    <v-card-title>{{ product.type }}</v-card-title>
                     <v-expand-transition>
                       <div
                         v-if="hover"
-                        class="d-flex transition-fast-in-fast-out white darken-2 v-card--reveal display-3 white--text"
-                        style="height: 100%;"
+                        class="
+                          d-flex
+                          transition-fast-in-fast-out
+                          white
+                          darken-2
+                          v-card--reveal
+                          display-3
+                          white--text
+                        "
+                        style="height: 100%"
                       >
-                        <v-btn v-if="hover" href="/product" class outlined>VIEW</v-btn>
+                        <v-btn
+                          v-if="hover"
+                          :href="`/product/${product.slug}`"
+                          class
+                          outlined
+                        >
+                          VIEW
+                        </v-btn>
                       </div>
                     </v-expand-transition>
                   </v-img>
                   <v-card-text class="text--primary">
                     <div>
-                      <a href="/product" style="text-decoration: none">{{pro.name}}</a>
+                      <a
+                        :href="`/product/${product.slug}`"
+                        style="text-decoration: none"
+                      >{{ product.name }}</a>
                     </div>
-                    <div>${{pro.price}}</div>
+                    <div>$ {{ product.price }}</div>
                   </v-card-text>
                 </v-card>
               </v-hover>
             </div>
           </div>
-          <div class="text-center mt-12">
-            <v-pagination v-model="page" :length="6"></v-pagination>
+          <div v-if="totalProducts > pageSize" class="text-center mt-12">
+            <v-pagination
+              v-model="page"
+              :length="paginationTabs"
+              @next="getProducts()"
+              @previous="getProducts()"
+              @input="getProducts()"
+            />
           </div>
         </div>
       </div>
@@ -144,6 +199,9 @@
 <script>
 export default {
   data: () => ({
+    paginationTabs: 0,
+    pageSize: 12,
+    totalProducts: 0,
     range: [0, 10000],
     select: 'Popularity',
     options: [
@@ -151,26 +209,10 @@ export default {
       'Popularity',
       'Relevance',
       'Price: Low to High',
-      'Price: High to Low',
+      'Price: High to Low'
     ],
     page: 1,
-    breadcrums: [
-      {
-        text: 'Home',
-        disabled: false,
-        href: 'breadcrumbs_home',
-      },
-      {
-        text: 'Clothing',
-        disabled: false,
-        href: 'breadcrumbs_clothing',
-      },
-      {
-        text: 'T-Shirts',
-        disabled: true,
-        href: 'breadcrumbs_shirts',
-      },
-    ],
+    breadcrums: [],
     min: 0,
     max: 10000,
     items: [
@@ -180,8 +222,8 @@ export default {
         children: [
           { id: 2, name: 'Casuals' },
           { id: 3, name: 'Formals' },
-          { id: 4, name: 'Sneakers' },
-        ],
+          { id: 4, name: 'Sneakers' }
+        ]
       },
       {
         id: 1,
@@ -190,75 +232,47 @@ export default {
           { id: 5, name: 'Shirts' },
           { id: 6, name: 'Tops' },
           { id: 7, name: 'Tunics' },
-          { id: 8, name: 'Bodysuit' },
-        ],
-      },
+          { id: 8, name: 'Bodysuit' }
+        ]
+      }
     ],
-    products: [
-      {
-        id: 1,
-        name: 'BLACK TEE',
-        type: 'Jackets',
-        price: '18.00',
-        src: 'assets/img/image_ph.jpg',
-      },
-      {
-        id: 2,
-        name: 'WHITE TEE',
-        type: 'Polo',
-        price: '40.00',
-        src: 'assets/img/image_ph.jpg',
-      },
-      {
-        id: 3,
-        name: 'Zara limited...',
-        type: 'Denim',
-        price: '25.00',
-        src: 'assets/img/image_ph.jpg',
-      },
-      {
-        id: 4,
-        name: 'SKULL TEE',
-        type: 'Jackets',
-        price: '30.00',
-        src: 'assets/img/image_ph.jpg',
-      },
-      {
-        id: 5,
-        name: 'MANGO WINTER',
-        type: 'Sweaters',
-        price: '50.00',
-        src: 'assets/img/image_ph.jpg',
-      },
-      {
-        id: 6,
-        name: 'SHIRT',
-        type: 'Denim',
-        price: '34.00',
-        src: 'assets/img/image_ph.jpg',
-      },
-      {
-        id: 7,
-        name: 'TRUCKER JACKET',
-        type: 'Jackets',
-        price: '38.00',
-        src: 'assets/img/image_ph.jpg',
-      },
-      {
-        id: 8,
-        name: 'COATS',
-        type: 'Jackets',
-        price: '25.00',
-        src: 'assets/img/image_ph.jpg',
-      },
-      {
-        id: 9,
-        name: 'MANGO WINTER',
-        type: 'Sweaters',
-        price: '50.00',
-        src: 'assets/img/image_ph.jpg',
-      },
-    ],
+    products: Array
   }),
+  mounted () {
+    this.getProductCatgoryMetaData()
+    this.getProducts()
+  },
+  methods: {
+    getProductCatgoryMetaData () {
+      const category = this.$route.params.slug
+      this.$axios
+        .get(`/categories/meta?cat=${category}`)
+        .then((res) => {
+          this.breadcrums = res.data.breadcrums
+        })
+        .catch((error) => {
+          console.log('Category Module. Error', error)
+        })
+    },
+    getProducts () {
+      const category = this.$route.params.slug
+      this.$axios
+        .get('/products/category', {
+          params: {
+            cat: category,
+            page: this.page,
+            pagesize: this.pageSize
+          }
+        })
+        .then((res) => {
+          this.products = res.data.products
+          this.totalProducts = res.data.total
+          this.paginationTabs = this.totalProducts / this.pageSize
+        })
+        .catch((error) => {
+          console.log('Category Module. Error', error)
+        })
+    }
+  }
 }
 </script>
